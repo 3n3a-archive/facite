120461f7e9f8899b0ec8ead960de7e64bc6780dc
3n3a <46775561+3n3a@users.noreply.github.com>
2020-06-09 17:45:05 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-wget file
+if [ `whoami` != root ]; then
+    echo Please run this script as root or using sudo
+    exit
+fi
+
+curl https://raw.githubusercontent.com/3n3a/facite/master/facite --output /usr/bin/facite
 chmod 777 /usr/bin/facite
+echo "Successfully installed facite. :)"
+echo "Visit my Website: 3n3a.ch"
